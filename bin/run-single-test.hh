4b022ab7b651508a0900ce5898815c78108306e8
Jason Orcutt <jorcutt@zynga.com>
2018-07-04 09:24:31 -0700

    moved all of the logic from run-single into the new class

--- bin/run-single-test.hh
+++ bin/run-single-test.hh
@@ -2,53 +2,15 @@
 <?hh
 
 $vendorDir = dirname(dirname(dirname(dirname(__FILE__))));
-$projectRoot = dirname(dirname($vendorDir));
+$projectRoot = dirname($vendorDir);
 
 require_once $vendorDir . '/autoload.php';
 
 use Zynga\RunSingleTest;
 
 $testRunner = new RunSingleTest(
-  $projectRoot
+  $projectRoot,
+  $argv
 );
 
-//$testRunner->run();
-
-
-$filePath = null;
-if ( isset($argv[1]) ) {
-  $filePath = $argv[1];
-} else {
-  $runner->usage('No test file path provided');
-}
-
-$testFunction = '';
-
-if ( isset($argv[2]) ) {
-  $testFunction = $argv[2];
-}
-
-$testName = '';
-
-if ( preg_match( '/\/([a-zA-Z0-9]*Test)\.hh$/', $filePath, $pregs) ) {
-  $testName = $pregs[1];
-} else {
-  $runner->usage("Expecting a path ending in <TestName>Test.hh");
-}
-
-$command = array();
-$command[] = $projectRoot . '/vendor/bin/phpunit';
-$command[] = '--debug';
-
-// add filter to get only a single function
-if ( $testFunction != '' ) {
-  $command[] = '--filter \'/::' . $testFunction . '$/\'';
-}
-
-$command[] = $testName;
-$command[] = $filePath;
-
-$cmd = join(' ', $command);
-
-echo "command: $cmd\n";
-passthru($cmd);
+$testRunner->run();
--- src/Zynga/RunSingleTest.hh
+++ src/Zynga/RunSingleTest.hh
@@ -3,8 +3,15 @@
 namespace Zynga;
 
 class RunSingleTest {
-  
-  public function usage($message) {
+  private string $projectRoot;
+  private array $argv;
+
+  public function __construct(string $projectRoot, array $argv) {
+    $this->projectRoot = $projectRoot;
+    $this->argv = $argv;
+  }
+
+  public function usage($message): void {
     echo "Error: $message\n";
     echo "\n";
     echo "run-single-test.hh <path>Test.hh [testFunction]\n";
@@ -12,6 +19,48 @@ class RunSingleTest {
     echo " Runs a single test suite handles the phpunit arguments\n";
     echo "\n";
     exit(255);
+  }
+
+  public function run(): void {
+
+    $filePath = null;
+    $testFunction = '';
+    $testName = '';
+
+    if ( isset($this->argv[1]) ) {
+      $filePath = $this->argv[1];
+    } else {
+      $this->usage('No test file path provided');
+    }
+
+    if ( isset($this->argv[2]) ) {
+      $testFunction = $this->argv[2];
+    }
+
+    $pregs = array();
+    if ( preg_match( '/\/([a-zA-Z0-9]*Test)\.hh$/', $filePath, $pregs) ) {
+      $testName = $pregs[1];
+    } else {
+      $this->usage("Expecting a path ending in <TestName>Test.hh");
+    }
+
+    $command = array();
+    $command[] = $this->projectRoot . '/vendor/bin/phpunit';
+    $command[] = '--debug';
+
+    // add filter to get only a single function
+    if ( $testFunction != '' ) {
+      $command[] = '--filter \'/::' . $testFunction . '$/\'';
+    }
+
+    $command[] = $testName;
+    $command[] = $filePath;
+
+    $cmd = implode(' ', $command);
+
+    echo "command: $cmd\n";
+    passthru($cmd);
+
   }
 
 }
