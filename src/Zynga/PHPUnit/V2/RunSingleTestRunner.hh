5ca5ff3859a99fe7c326dd2897ee7756f867cf15
Jason Orcutt <jorcutt@zynga.com>
2018-07-27 21:47:05 -0700

    working on cleaning up the xhprof implementation

---
+++ src/Zynga/PHPUnit/V2/RunSingleTestRunner.hh
@@ -0,0 +1,59 @@
+<?hh // strict
+
+namespace Zynga\PHPUnit\V2;
+
+use Zynga\PHPUnit\V2\Runner;
+
+class RunSingleTestRunner extends Runner {
+
+  public function scriptName(): string {
+    return strval($this->argv->get(0));
+  }
+
+  public function usage(string $message): void {
+    echo "Error: $message\n";
+    echo "\n";
+    echo $this->scriptName()." <path>Test.hh [testFunction]\n";
+    echo "\n";
+    echo " Runs a single test suite handles the phpunit arguments\n";
+    echo "\n";
+    exit(255);
+  }
+
+  public function createPHPUnitArgStack(): Vector<string> {
+
+    if (!$this->argv->containsKey(1)) {
+      $this->usage('No test file path provided');
+    }
+
+    $filePath = strval($this->argv->get(1));
+
+    $testFunction = '';
+    if ($this->argv->containsKey(2)) {
+      $testFunction = strval($this->argv->get(2));
+    }
+
+    $testName = '';
+    $pregs = array();
+    if (preg_match('/\/([a-zA-Z0-9]*Test)\.hh$/', $filePath, $pregs)) {
+      $testName = $pregs[1];
+    } else {
+      $this->usage("Expecting a path ending in <TestName>Test.hh");
+    }
+
+    $argStack = parent::createPHPUnitArgStack();
+
+    $argStack->add('--debug');
+
+    if ($testFunction != '') {
+      $argStack->add('--filter \'/::'.$testFunction.'$/\'');
+    }
+
+    $argStack->add($testName);
+    $argStack->add($filePath);
+
+    return $argStack;
+
+  }
+
+}
