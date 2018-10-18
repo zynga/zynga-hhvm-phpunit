0c684d3664e0707ad0ee7d717e0f1b0c2540c3b2
Jason Orcutt <jorcutt@zynga.com>
2018-10-18 22:18:21 +0000

    cable release bump

--- src/Zynga/PHPUnit/V2/Version.hh
+++ src/Zynga/PHPUnit/V2/Version.hh
@@ -5,11 +5,13 @@ namespace Zynga\PHPUnit\V2;
 // --
 // We utilize superheros for our major releases:
 // - 1.8.x : ajax
+// - 1.9.x : blade
+// - 2.0.0 : cable
 //
 // --
 class Version {
-  const string VERSION_NUMBER = "1.9.6";
-  const string VERSION_NAME   = "blade";
+  const string VERSION_NUMBER = "2.0.0";
+  const string VERSION_NAME   = "cable";
 
   public static function get(): string {
     return self::VERSION_NUMBER . ' - ' . self::VERSION_NAME;
