4221507446fc3cbc32a0342271a5cdf2c1c3c1dd
Jason Orcutt <jorcutt@zynga.com>
2019-02-19 10:28:20 -0800

    more migration of testcase code to hacklang

---
+++ src/Zynga/PHPUnit/V2/TestCase/Size.hh
@@ -0,0 +1,11 @@
+<?hh // strict
+
+namespace Zynga\PHPUnit\V2\TestCase;
+
+class Size {
+  const int UNDETERMINED = -2;
+  const int UNKNOWN = -1;
+  const int SMALL = 0;
+  const int MEDIUM = 1;
+  const int LARGE = 2;
+}
